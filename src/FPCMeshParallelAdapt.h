1d446b60ba136e34621409fdf4ba29cf103a3ed0
Kazimierz Michalik <kazimierz.michalik@gmail.com>
2016-12-20 23:07:49 +0100

    Parallel adapt mesh class

---
+++ src/FPCMeshParallelAdapt.h
@@ -0,0 +1,15 @@
+#ifndef FPCMESH_PARALLEL_ADAPT_HEADER
+#define FPCMESH_PARALLEL_ADAPT_HEADER
+
+#include "FPCMesh.h"
+
+namespace FPCM {
+
+class FPCMeshParallelAdapt  : public FPCMeshParallel, FPCMeshAdapt
+{
+public:
+}; //! FPCMeshParallel
+
+} //! FPCM
+
+#endif //!FPCMESH_PARALLEL_ADAPT_HEADER
